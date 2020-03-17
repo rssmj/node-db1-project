5c97059d8fafe5e138bf72c645ce7e693cbac4c5
Michael Ross <mross.j@gmail.com>
2020-03-17 03:31:02 -0400

    add GET endpoints for accounts
    * added GET for:
    * accounts from db
    * order by id, desc, limit 7
    * order by budget, desc, limit 11
    * count by name
    * account by id

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -1,8 +1,90 @@
-const express = require("express");
+const express = require('express');
 
-// connection to the database
+// connection to database using Knex
 const db = require('../data/dbConfig.js');
 
 const router = express.Router();
+
+// get accounts from db
+router.get('/', (req, res) => {
+	// select * from accounts,
+	db.select('*')
+		.from('accounts') // returns promise
+		.then(rows => {
+			res.status(200).json({ data: rows });
+		})
+		.catch(() => {
+			res.status(500).json({ response: 'error, try something else' });
+		});
+});
+
+// order by id, desc, limit 7
+router.get('/order/id', (req, res) => {
+	const { id } = req.params;
+	const limit = req.query.limit || 7;
+	// const limit = (req.query.limit = 7);
+	const sortBy = req.query.sortBy || 'id';
+	// const sortBy = (req.query.sortBy = 'id');
+	const sortDir = req.query.sortDir || 'desc';
+	// const sortDir = (req.query.sortDir = 'desc');
+	db.select(id, '*')
+		.from('accounts')
+		.limit(limit)
+		.orderBy(sortBy, sortDir)
+		.then(order => {
+			res.status(200).json({ data: order });
+		})
+		.catch(() => {
+			res.status(500).json({ response: `error ordering accounts by 'id'` });
+		});
+});
+
+// order by budget, desc, limit 11
+router.get('/order/budget', (req, res) => {
+	const limit = req.query.limit || 11;
+	const sortBy = req.query.sortBy || 'budget';
+	const sortDir = req.query.sortDir || 'desc';
+	db.select('*')
+		.from('accounts')
+		.orderBy(sortBy, sortDir)
+		.limit(limit)
+		.then(budgetOrder => {
+			res.status(200).json({ data: budgetOrder });
+		})
+		.catch(() => {
+			res.status(500).json({ response: `error ordering accounts by 'budget'` });
+		});
+});
+
+// count by name
+router.get('/count/name', (req, res) => {
+	const countBy = req.query.countBy || 'name';
+	db('accounts')
+		.count(countBy, { as: 'nameCount' })
+		.then(nameCount => {
+			res.status(200).json({ data: nameCount });
+		})
+		.catch(() => {
+			res.status(500).json({ response: `error ordering accounts by 'name'` });
+		});
+});
+
+// get by 'id'
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	db('accounts')
+		.where({ id })
+		// .where("id", "=", req.params.id)
+		// .where({ id: req.params.id })
+		.first() // gets first element
+		.then(account => {
+			account
+				? res.status(200).json({ data: account })
+				: res.status(404).json({ response: 'hmm, no account found' });
+		})
+		.catch(() => {
+			res.status(500).json({ response: 'errors happened' });
+		});
+});
 
 module.exports = router;
